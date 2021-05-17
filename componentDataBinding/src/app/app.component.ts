d794726ca1b4450fe34498d1b8599b86dc8bf6dc
DESKTOP-8TBEVD4\Krystian Izdebski <krystian.izdebski1999@gmail.com>
2021-05-17 19:05:37 +0200

    Sending data from parent to child component example

--- componentDataBinding/src/app/app.component.ts
+++ componentDataBinding/src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'componentDataBinding';
+
+  dataBind: String;
+  secondDataBind: String;
 }
---
+++ componentDataBinding/src/app/first-component/first-component.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-first-component',
+  templateUrl: './first-component.component.html',
+  styleUrls: ['./first-component.component.css']
+})
+export class FirstComponentComponent implements OnInit {
+
+  @Input() secondDataBind: String;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
